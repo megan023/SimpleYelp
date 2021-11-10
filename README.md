63ac3117754e7cde1229a3278835c15f9df4d887
Megan Liu <meganliu@stanford.edu>
2021-11-10 12:33:23 -0800

    search function + README

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# SimpleYelp
+#Yelp Clone
+
+## Megan Liu
+
+**Yelp clone** displays a list of search results from the Yelp API and displays
+the results in a scrollable list. 
+
+Time spent: **4** hours spent in total
+
+## Functionality 
+
+The following **required** functionality is completed:
+
+* [x] Ability to query the Yelp API to get results from a search query
+* [x] The search results are displayed in a RecyclerView
+
+The following **extensions** are implemented:
+
+* [x] Added a search menu option to the app bar. 
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://i.imgur.com/y1UTcRE.gif' title='Video
+Walkthrough' width='' alt='Video Walkthrough' />
+
+Recorded with QuickTimePlayer and converted to GIF with EZGIF.COM.
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright 2021 Megan Liu
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+            you may not use this file except in compliance with the License.
+                You may obtain a copy of the License at
+
+                        http://www.apache.org/licenses/LICENSE-2.0
+
+                            Unless required by applicable law or agreed to in
+                            writing, software
+                                distributed under the License is distributed on
+                                an "AS IS" BASIS,
+                                    WITHOUT WARRANTIES OR CONDITIONS OF ANY
+                                    KIND, either express or implied.
+                                        See the License for the specific
+                                        language governing permissions and
+                                            limitations under the License.
--- app/src/main/java/edu/stanford/meganliu/simpleyelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/meganliu/simpleyelp/MainActivity.kt
@@ -1,8 +1,13 @@
 package edu.stanford.meganliu.simpleyelp
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -16,22 +21,51 @@ private const val BASE_URL = "https://api.yelp.com/v3/"
 private const val API_KEY = "uLos4-6vDxIzNCv4v5DcMx5JkPw0VOdq8nZAtreswOr2pOY-3I5tnLNDLXIklZ7q3hl_AiZWd7T2qpFYUHjYRwSUG3IDk43ARuW9ngNIGNMugxWJf92-AKVE6WWLYXYx"
 class MainActivity : AppCompatActivity() {
     private lateinit var rvRestaurants: RecyclerView
+    private val restaurants = mutableListOf<YelpRestaurant>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rvRestaurants = findViewById(R.id.rvRestaurants)
-        val restaurants = mutableListOf<YelpRestaurant>()
+
         val adapter = RestaurantsAdapter(this, restaurants)
         rvRestaurants.adapter = adapter
         rvRestaurants.layoutManager = LinearLayoutManager(this)
 
+        //queryData("Avocado Toast")
+        handleIntent(intent)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            Log.i(TAG, "Received intent to search for $query")
+            //use the query to search your data somehow
+            queryData(query.toString())
+        }
+    }
+    private fun queryData(searchTerm: String){
         val retrofit =
             Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
                 .build()
         val yelpService =retrofit.create(YelpService::class.java)
-        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult>{
+        yelpService.searchRestaurants("Bearer $API_KEY", searchTerm, "New York").enqueue(object : Callback<YelpSearchResult>{
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse $response")
                 val body = response.body()
@@ -39,15 +73,15 @@ class MainActivity : AppCompatActivity() {
                     Log.w(TAG, "Did not receive valid body from Yelp API... exiting")
                     return
                 }
+                restaurants.clear()
                 restaurants.addAll(body.restaurants)
                 Log.i(TAG, "$restaurants")
-                adapter.notifyDataSetChanged()
+                rvRestaurants.adapter?.notifyDataSetChanged()
             }
 
             override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
                 Log.i(TAG, "onFailure $t")
             }
-
         })
     }
 }
