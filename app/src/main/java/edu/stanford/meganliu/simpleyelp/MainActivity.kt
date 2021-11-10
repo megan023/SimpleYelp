20bf3fb53684960000bb460422a82ed0c4ad729d
Megan Liu <meganliu@stanford.edu>
2021-11-10 00:12:24 -0800

    Creating and Implementing the RecyclerView

--- app/src/main/java/edu/stanford/meganliu/simpleyelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/meganliu/simpleyelp/MainActivity.kt
@@ -3,6 +3,8 @@ package edu.stanford.meganliu.simpleyelp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,9 +15,18 @@ private const val TAG = "MainActivity"
 private const val BASE_URL = "https://api.yelp.com/v3/"
 private const val API_KEY = "uLos4-6vDxIzNCv4v5DcMx5JkPw0VOdq8nZAtreswOr2pOY-3I5tnLNDLXIklZ7q3hl_AiZWd7T2qpFYUHjYRwSUG3IDk43ARuW9ngNIGNMugxWJf92-AKVE6WWLYXYx"
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvRestaurants: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvRestaurants = findViewById(R.id.rvRestaurants)
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantsAdapter(this, restaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
         val retrofit =
             Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
                 .build()
@@ -23,6 +34,13 @@ class MainActivity : AppCompatActivity() {
         yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult>{
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if (body ==null){
+                    Log.w(TAG, "Did not receive valid body from Yelp API... exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
             }
 
             override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
---
+++ app/src/main/java/edu/stanford/meganliu/simpleyelp/RestaurantsAdapter.kt
@@ -0,0 +1,33 @@
+package edu.stanford.meganliu.simpleyelp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+//import kotlinx.android.synthetic.main.item_restaurant.view.*
+
+class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
+    RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_restaurant, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val restaurant = restaurants[position]
+        holder.bind(restaurant);
+    }
+
+    override fun getItemCount() = restaurants.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val tvName = itemView.findViewById<TextView>(R.id.tvName);
+
+        fun bind(restaurant: YelpRestaurant) {
+            tvName.text = restaurant.name
+        }
+    }
+
+}
